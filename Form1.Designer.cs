21276d9e8b99c4909de53042caba5b9eb13d8fe0
Jamil Nakhleh <mr.nakhle@gmail.com>
2023-09-28 22:18:57 +0300

    Add files via upload

---
+++ Form1.Designer.cs
@@ -0,0 +1,185 @@
+﻿namespace Tarteeb_App
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.button1 = new System.Windows.Forms.Button();
+            this.label1 = new System.Windows.Forms.Label();
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.button2 = new System.Windows.Forms.Button();
+            this.menuStrip1 = new System.Windows.Forms.MenuStrip();
+            this.fileToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.closeToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.treeView_before = new System.Windows.Forms.TreeView();
+            this.treeView_after = new System.Windows.Forms.TreeView();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.menuStrip1.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(630, 118);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(111, 52);
+            this.button1.TabIndex = 0;
+            this.button1.Text = "Organize";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(39, 74);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(91, 20);
+            this.label1.TabIndex = 1;
+            this.label1.Text = "Folder Path";
+            // 
+            // textBox1
+            // 
+            this.textBox1.Location = new System.Drawing.Point(138, 68);
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(603, 26);
+            this.textBox1.TabIndex = 2;
+            // 
+            // button2
+            // 
+            this.button2.Location = new System.Drawing.Point(138, 118);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(111, 52);
+            this.button2.TabIndex = 5;
+            this.button2.Text = "Browse";
+            this.button2.UseVisualStyleBackColor = true;
+            this.button2.Click += new System.EventHandler(this.button2_Click);
+            // 
+            // menuStrip1
+            // 
+            this.menuStrip1.ImageScalingSize = new System.Drawing.Size(24, 24);
+            this.menuStrip1.Items.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.fileToolStripMenuItem});
+            this.menuStrip1.Location = new System.Drawing.Point(0, 0);
+            this.menuStrip1.Name = "menuStrip1";
+            this.menuStrip1.Size = new System.Drawing.Size(929, 33);
+            this.menuStrip1.TabIndex = 6;
+            this.menuStrip1.Text = "menuStrip1";
+            // 
+            // fileToolStripMenuItem
+            // 
+            this.fileToolStripMenuItem.DropDownItems.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.closeToolStripMenuItem});
+            this.fileToolStripMenuItem.Name = "fileToolStripMenuItem";
+            this.fileToolStripMenuItem.Size = new System.Drawing.Size(54, 50);
+            this.fileToolStripMenuItem.Text = "File";
+            this.fileToolStripMenuItem.Click += new System.EventHandler(this.fileToolStripMenuItem_Click);
+            // 
+            // closeToolStripMenuItem
+            // 
+            this.closeToolStripMenuItem.Name = "closeToolStripMenuItem";
+            this.closeToolStripMenuItem.Size = new System.Drawing.Size(157, 34);
+            this.closeToolStripMenuItem.Text = "Close";
+            this.closeToolStripMenuItem.Click += new System.EventHandler(this.closeToolStripMenuItem_Click);
+            // 
+            // treeView_before
+            // 
+            this.treeView_before.Location = new System.Drawing.Point(87, 241);
+            this.treeView_before.Name = "treeView_before";
+            this.treeView_before.Size = new System.Drawing.Size(275, 492);
+            this.treeView_before.TabIndex = 7;
+            // 
+            // treeView_after
+            // 
+            this.treeView_after.Location = new System.Drawing.Point(544, 241);
+            this.treeView_after.Name = "treeView_after";
+            this.treeView_after.Size = new System.Drawing.Size(275, 492);
+            this.treeView_after.TabIndex = 8;
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label2.Location = new System.Drawing.Point(169, 194);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(137, 44);
+            this.label2.TabIndex = 9;
+            this.label2.Text = "Before";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label3.Location = new System.Drawing.Point(652, 194);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(101, 44);
+            this.label3.TabIndex = 10;
+            this.label3.Text = "After";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(9F, 20F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(929, 774);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.treeView_after);
+            this.Controls.Add(this.treeView_before);
+            this.Controls.Add(this.button2);
+            this.Controls.Add(this.textBox1);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.menuStrip1);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.Fixed3D;
+            this.MainMenuStrip = this.menuStrip1;
+            this.MaximizeBox = false;
+            this.Name = "Form1";
+            this.ShowIcon = false;
+            this.Text = "Tarteeb_App";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.menuStrip1.ResumeLayout(false);
+            this.menuStrip1.PerformLayout();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.MenuStrip menuStrip1;
+        private System.Windows.Forms.ToolStripMenuItem fileToolStripMenuItem;
+        private System.Windows.Forms.TreeView treeView_before;
+        private System.Windows.Forms.TreeView treeView_after;
+        private System.Windows.Forms.ToolStripMenuItem closeToolStripMenuItem;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label3;
+    }
+}
+
---
+++ Form1.cs
@@ -0,0 +1,179 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Tarteeb_App
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        string folderPath = "";
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+        
+        }
+
+        private void OrganizeFilesInFolder(string folderPath)
+        {
+            try
+            {
+                string[] files = Directory.GetFiles(folderPath);
+
+                foreach (string filePath in files)
+                {
+                    string fileName = Path.GetFileName(filePath);
+                    string fileExtension = Path.GetExtension(filePath).ToLower();
+                    string destinationFolder = Path.Combine(folderPath, fileExtension.TrimStart('.'));
+
+                    // creating the destination folder if it does NOT exist
+                    Directory.CreateDirectory(destinationFolder);
+
+                    // moving the file to the des folder
+                    string newFilePath = Path.Combine(destinationFolder, fileName);
+                    File.Move(filePath, newFilePath);
+                }
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show($"Error organizing files: {ex.Message}", "Error", MessageBoxButtons.OK, MessageBoxIcon.Error);
+            }
+        }
+        
+        private void PopulateTreeView(string directory, TreeNodeCollection parentNode)
+        {
+            // Enumerate files
+            string[] files = Directory.GetFiles(directory);
+            foreach (string file in files)
+            {
+                TreeNode node = new TreeNode(Path.GetFileName(file));
+                parentNode.Add(node);
+            }
+
+            // Enumerate subdirectories
+            string[] subDirectories = Directory.GetDirectories(directory);
+            foreach (string subDirectory in subDirectories)
+            {
+                TreeNode node = new TreeNode(Path.GetFileName(subDirectory));
+                parentNode.Add(node);
+
+                // Recursively populate subdirectories
+                PopulateTreeView(subDirectory, node.Nodes);
+            }
+        }
+
+        static string ShortenFolderPath(string folderPath, int maxLength)
+        {
+            string[] pathParts = folderPath.Split('\\');
+            int partsCount = pathParts.Length;
+
+            if (partsCount > 0)
+            {
+                for (int i = 0; i < partsCount; i++)
+                {
+                    if (pathParts[i].Length > maxLength)
+                    {
+                        // Abbreviate directory name to maxLength characters
+                        pathParts[i] = pathParts[i].Substring(0, maxLength);
+                    }
+                }
+
+                // Reconstruct the shortened path
+                string shortenedPath = string.Join("\\", pathParts);
+                return shortenedPath;
+            }
+            else
+            {
+                // If the path is empty, return it as is
+                return folderPath;
+            }
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            textBox1.Text = "";
+          
+            FolderBrowserDialog folderBrowserDialog = new FolderBrowserDialog();
+
+           
+            folderBrowserDialog.SelectedPath = Environment.GetFolderPath(Environment.SpecialFolder.MyDocuments);
+
+
+            
+            DialogResult result = folderBrowserDialog.ShowDialog();
+
+           
+            if (result == DialogResult.OK)
+            {
+          
+                string selectedFolderPath = folderBrowserDialog.SelectedPath;
+ 
+                textBox1.Text =  selectedFolderPath;
+                treeView_before.Nodes.Clear();
+                treeView_after.Nodes.Clear();
+
+                PopulateTreeView(textBox1.Text, treeView_before.Nodes);
+            }
+            else
+            {
+                textBox1.Text = "Error selection";
+            }
+        }
+        
+        private void button3_Click(object sender, EventArgs e)
+        {
+            
+        }
+
+        private void fileToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void shortPathToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            
+        }
+       
+
+        private void closeToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            this.Close();
+        }
+
+        private void shortPathToolStripMenuItem_Click_1(object sender, EventArgs e)
+        {
+
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+          
+            folderPath = textBox1.Text;
+
+            if (Directory.Exists(folderPath))
+            {
+                OrganizeFilesInFolder(folderPath);
+                MessageBox.Show("The files organized successfully!", "Tarteeb_App", MessageBoxButtons.OK, MessageBoxIcon.Information);
+
+                PopulateTreeView(folderPath, treeView_after.Nodes);
+            }
+            else
+            {
+                MessageBox.Show("Folder does NOT exist!", "Error", MessageBoxButtons.OK, MessageBoxIcon.Error);
+            }
+        }
+    }
+}
